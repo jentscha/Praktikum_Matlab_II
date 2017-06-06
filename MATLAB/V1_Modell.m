db0941c811d915458685c89cba58132d771b26ef
merekila <alkesaca@gmail.com>
2017-06-06 14:02:15 +0200

    Fehler ausgemerzt

--- MATLAB/V1_Modell.m
+++ MATLAB/V1_Modell.m
@@ -1,15 +1,25 @@
-clear
+clear all
 clc
 %% Variablen
 % Parameter
 syms m1 l_1 J1 m2 l_2 J2 Rp1 Rp2 g t positive;
 
 % Koordinaten
-syms x1 dx1 ddx1 y1 dy1 ddy1 phi1 dphi1 ddphi1...
-    x2 dx2 ddx2 y2 dy2 ddy2 phi2 dphi2 ddphi2 real;
+% syms x1 dx1 ddx1 y1 dy1 ddy1 phi1 dphi1 ddphi1...
+%     x2 dx2 ddx2 y2 dy2 ddy2 phi2 dphi2 ddphi2 real;
+syms phi1 dphi1 ddphi1 phi2 dphi2 ddphi2 real;
 
 % Momente
 syms M MR1 MR2 real;
+
+% %% Werte einsetzen
+% l_1 = subs(l_1, 0.2);
+% l_2 = subs(l_2, 0.2);
+% g = subs(g, 9.81);
+% m1 = subs(m1, 0.3);
+% m2 = subs(m2, 0.3);
+% Rp1 = subs(Rp1, 1e-2);
+% Rp2 = subs(Rp1, 1e-3);
 
 %% Zu ersetzende Größen
 
@@ -24,30 +34,30 @@ J2 = 1 / 12 * m2 * l_2^2;
 % Pendelkoordinaten
 x1 = l_1 / 2 * sin(phi1);
 dx1 = l_1 / 2 * dphi1 * cos(phi1);
-ddx1 = l_1 / 2 * (ddphi1 * cos(phi1) - dphi1^2 * sin(phi1));
+%ddx1 = l_1 / 2 * (ddphi1 * cos(phi1) - dphi1^2 * sin(phi1));
 
 y1 = -l_1 / 2 * cos(phi1);
 dy1 = l_1 / 2 * dphi1 * sin(phi1);
-ddy1 = l_1 / 2 * (ddphi1 * sin(phi1) + dphi1^2 * cos(phi1));
+%ddy1 = l_1 / 2 * (ddphi1 * sin(phi1) + dphi1^2 * cos(phi1));
 
-x2 = 2 * x1 + l_2 / 2 * sin(phi1+phi2);
-dx2 = 2 * dx1 + l_2 / 2 * (dphi1 + dphi2) * cos(phi1 + phi2);
-ddx2 = 2 * ddx1 + l_2 / 2 * ((ddphi1 + ddphi2) * cos(phi1 + phi2) -...
-                                    (dphi1 + dphi2)^2 * sin(phi1 + phi2));
+x2 =  x1 + l_2 / 2 * sin(phi2);
+dx2 = dx1 + l_2 / 2 * dphi2 * cos(phi2);
+%ddx2 = ddx1 + l_2 / 2 * ((ddphi1 + ddphi2) * cos(phi1 + phi2) -...
+%                                    (dphi1 + dphi2)^2 * sin(phi1 + phi2));
 
-y2 = 2 * y1 - l_2 / 2 * cos(phi1 + phi2);
-dy2 = 2 * dy1 + l_2 / 2 * (dphi1 + dphi2) * sin(phi1 + phi2);
-ddy2 = 2* ddy1 + l_2 / 2 * ((ddphi1 + ddphi2) * sin(phi1 + phi2) +...
-                                    (dphi1 + dphi2)^2 * cos(phi1 + phi2));
+y2 = y1 - l_2 / 2 * cos(phi2);
+dy2 = dy1 + l_2 / 2 * dphi2 * sin(phi2);
+%ddy2 = 2* ddy1 + l_2 / 2 * ((ddphi1 + ddphi2) * sin(phi1 + phi2) +...
+%                                    (dphi1 + dphi2)^2 * cos(phi1 + phi2));
                                 
 %% Mechanik
 
 T = 1/2 * m1 * (dx1^2 + dy1^2) + 1/2 * J1 * dphi1^2 +...
     1/2 * m2 * (dx2^2 + dy2^2) + 1/2 * J2 * dphi2^2;
 
-U = g * (m1 * y1 + m2 * y2);
-% U = -m1 * g * l_1 / 2 * cos(phi1) -...
-%     m2 * g * (l_1 * cos(phi1) + l_2 / 2  * cos(phi1 + phi2));
+%U = g * (m1 * y1 + m2 * y2);
+U = -m1 * g * l_1 / 2 * cos(phi1) -...
+     m2 * g * (l_1 * cos(phi1) + l_2 / 2  * cos(phi1 + phi2));
 
 %Generalisierte Kräfte
 
@@ -95,16 +105,13 @@ Var_ot = {phi1, dphi1, dphi1, ddphi1, phi2, dphi2, dphi2, ddphi2};
 dL_dphi1_t = subs(dL_dphi1_t, Var_t, Var_ot);
 dL_dphi2_t = subs(dL_dphi2_t, Var_t, Var_ot);
 
-%% Werte einsetzen
-l_1 = subs(l_1, 0.2);
-l_2 = subs(l_2, 0.2);
-g = subs(g, 9.81);
-m1 = subs(m1, 0.3);
-m2 = subs(m2, 0.3);
-Rp1 = subs(Rp1, 1e-2);
-Rp2 = subs(Rp1, 1e-3);
 
 %% Berchnung der LAGRANGEschen Gleichungen
 
 Sol = solve([dL_dphi1_t - L_phi1 == Qphi1, dL_dphi2_t - L_phi2 == Qphi2],...
             [ddphi1, ddphi2]);
+Sol.ddphi1=simplify(Sol.ddphi1);
+Sol.ddphi2=simplify(Sol.ddphi2);
+pretty(Sol.ddphi1);
+pretty(Sol.ddphi2);
+
