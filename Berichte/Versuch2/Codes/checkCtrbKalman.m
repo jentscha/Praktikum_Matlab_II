902d7e74283e32737be25cd3a32848023be7cf72
AJentsch <jentscha@yahoo.com>
2017-06-17 15:36:56 +0200

    fügt diverse matritzen in bericht v2 ein

--- Berichte/Versuch2/Codes/checkCtrbKalman.m
+++ Berichte/Versuch2/Codes/checkCtrbKalman.m
@@ -3,15 +3,13 @@ function [ S_s_Kalman] = checkCtrbKalman( A, B )
 n = length(A);
 
 if (rank(A)==n)
-    S_s_Kalman = B;
-    for count = 1:n-1
-        S_s_Kalman = [S_s_Kalman, A^count *B ];
-    end
-    if rank(S_s_Kalman) == n
-        disp('System ist steuerbar nach Kalman');
-    end
+    
 else
     disp('Matrix A hat nicht vollen Rang')
+end
+S_s_Kalman = B;
+for count = 1:n-1
+    S_s_Kalman = [S_s_Kalman, A^count *B ];
 end
 end
 
--- Berichte/Versuch2/Codes/checkObsvKalman.m
+++ Berichte/Versuch2/Codes/checkObsvKalman.m
@@ -3,15 +3,12 @@ function [ S_b_Kalman ] = checkObsvKalman( A, C )
 n = length(A);
 
 if rank(A) == n
-    S_b_Kalman = C;
-    for count = 1:n-1
-        S_b_Kalman = [S_b_Kalman ; C * A^count];
-    end
-    if rank(S_B_Kalman == n
-        disp('System ist beobachtbar nach Kalman');
-    end
 else    
     disp('Matrix A hat nicht vollen Rang');
+end
+S_b_Kalman = C;
+for count = 1:n-1
+    S_b_Kalman = [S_b_Kalman ; C * A^count];
 end
 end
 
