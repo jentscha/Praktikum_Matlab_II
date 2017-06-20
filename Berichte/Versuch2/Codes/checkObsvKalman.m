fd48c37e6967fa922696563c763ca9bf4bcb01f8
AJentsch <jentscha@yahoo.com>
2017-06-20 12:41:15 +0200

    hoffentlich alles geändert und gerettet

--- Berichte/Versuch2/Codes/checkObsvKalman.m
+++ Berichte/Versuch2/Codes/checkObsvKalman.m
@@ -5,13 +5,10 @@ n = length(A);
 if rank(A) == n
 else    
     disp('Matrix A hat nicht vollen Rang');
-<<<<<<< HEAD
-=======
 end
 S_b_Kalman = C;
 for count = 1:n-1
     S_b_Kalman = [S_b_Kalman ; C * A^count];
->>>>>>> 38b7d0387000b3368fdeffdc189b3b80731375e2
 end
 end
 
