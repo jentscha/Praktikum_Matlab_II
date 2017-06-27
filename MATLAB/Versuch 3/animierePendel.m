2be9b1a2072c3d19df4ed8904cfea1f60d5b6c6b
merekila <alkesaca@gmail.com>
2017-06-27 14:30:16 +0200

    finalisiert Bericht

--- MATLAB/Versuch 3/animierePendel.m
+++ MATLAB/Versuch 3/animierePendel.m
@@ -17,18 +17,20 @@ if isempty(hAxes)
     figure;
     hAxes = axes();
 end
-axis([-2 2 -2 2])
+axis([-2 2 -2 2]*0.2)
 
 hold on;
 for i = 1:nBilder
     if i>1
         delete(hPendel);
     end
-    P1 = [sin(mXAnim(i,1)), -cos(mXAnim(i,1))];
-    P2 = P1 + [sin(mXAnim(i,3)), -cos(mXAnim(i,3))];
+    P1 = [sin(mXAnim(i,1)), -cos(mXAnim(i,1))]*0.2;
+    P2 = P1 + [sin(mXAnim(i,3)), -cos(mXAnim(i,3))]*0.2;
     hPendel = plot(hAxes, [0, P1(1)],[0, P1(2)],'b',...
                     [P1(1), P2(1)], [P1(2), P2(2)],'r');
-    title(num2str(vTAnim(i)));
+    title(['t = ',num2str(vTAnim(i)),' sec']);
+    xlabel('x[m]');
+    ylabel('y[m]');
     
     if MakeAvi
         if i == 1
