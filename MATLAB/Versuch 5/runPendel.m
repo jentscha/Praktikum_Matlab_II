cf7fac73319bc1c6baac134ba8b831ec511abe64
AJentsch <jentscha@yahoo.com>
2017-07-04 22:59:38 +0200

    stellt testscript.m fertig für plotten der sachen für den bericht

--- MATLAB/Versuch 5/runPendel.m
+++ MATLAB/Versuch 5/runPendel.m
@@ -1,11 +1,11 @@
-function [ sim ] = runPendel( stPendel, AP, x0, M_AP, Tend )
+function [ simOut ] = runPendel( stPendel, AP, x0, M_AP, stTraj, Tend )
 
 stOptions = simset( 'SrcWorkspace', 'current');
 sim('Modell_V5', Tend, stOptions);
 
-sim.vT = mZustand.Time;
-sim.mX = mZustand.Data;
-sim.vU = vInput.Data;
+simOut.vT = mZustand.Time;
+simOut.mX = mZustand.Data;
+simOut.vU = vInput.Data;
 
 end
 
