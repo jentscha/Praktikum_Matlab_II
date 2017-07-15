894551eddfe7731a22ec1fa2d67d13195892a064
merekila <alkesaca@gmail.com>
2017-07-15 20:28:25 +0200

    fängt mit Bericht an

--- Berichte/Versuch6/Codes/linearisierung_XU.m
+++ Berichte/Versuch6/Codes/linearisierung_XU.m
@@ -5,21 +5,17 @@ syms phi1 phi2 dphi1 dphi2 ddphi1 ddphi2 M;
 [f,h] = nonlinear_model();
 
 z = [phi1;dphi1;phi2;dphi2];
-u = M;
-
-f_M_AP = subs(f(2),z,x);
-
-M_AP = solve(f_M_AP == 0 , M);
+u_z = M;
 
 A = jacobian(f,z);
-B = jacobian(f,u);
+B = jacobian(f,u_z);
 C = jacobian(h,z);
-D = jacobian(h,u);
+D = jacobian(h,u_z);
 
-A = subs(A,[z,u],[x(1),M_AP]);
-B = subs(B,[z,u],[AP,M_AP]);
-C = subs(C,[z,u],[AP,M_AP]);
-D = subs(D,[z,u],[AP,M_AP]);
+A = subs(A,[z.',u_z],[x.',u]);
+B = subs(B,[z.',u_z],[x.',u]);
+C = subs(C,[z.',u_z],[x.',u]);
+D = subs(D,[z.',u_z],[x.',u]);
 
 
 A = double(A);
